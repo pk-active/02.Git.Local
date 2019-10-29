d78f18345b503d90a3e3dbce1a2dcb3f5656358d
Kirill Pavlov <kirill.pavlov@activecloud.com>
2019-10-29 12:31:46 +0300

    Modification of the push automation script

--- push-all.sh
+++ push-all.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo 'Push project to Github repository'
 git push -u origin --all
+echo 'Push project to Bitbucket repository'
 git push -u bitbucket --all
+echo 'Push project to GitLub repository'
 git push -u gitlab --all
+echo 'All done!'
